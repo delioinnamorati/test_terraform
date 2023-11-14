c0af9fc1db7cbeaf62d954fba626b691271439e5
Delio <>
2023-11-14 17:41:49 +0000

    add changes

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_version = "~>1.5.0"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.10.0"
+    }
+  }
+}
 provider "aws" {
     region = "us-east-1"
     #assume_role {
@@ -9,5 +21,6 @@ terraform {
 
   }
 }
-block = "10.0.0.0/16"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
