5e34b44d9dd3593cbc5fbf80c4cdad9de9b8377e
Delio <>
2023-11-14 19:07:44 +0000

    add updates

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = "~>1.5.0"
+  required_version = "~>1.6.0"
 }
 
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~>5.10.0"
+      version = "~>5.25.0"
     }
   }
 }
