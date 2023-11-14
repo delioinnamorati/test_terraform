dc239ea25b74943319505f4429d1461d9fc3a552
Delio <>
2023-11-14 17:30:52 +0000

    test2

---
+++ environment/prod/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+}
+
+variable "dynamodb_table" {
+}
+variable "account_id" {
+
+}
+variable "bucket" {
+  default = ""
+}
+variable "role" {
+  
+}
+variable "encrypt" {
+
+}
+variable "key" {
+
+}
--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
+<<<<<<< Updated upstream
+=======
+provider "aws" {
+    region = "us-east-1"
+    #assume_role {
+        #role_arn = "arn:aws:iam::${var.account_id}:role/${var.role}"
+    #}
+}
+terraform {
+  backend "s3" {
+
+  }
+}
+/*
+terraform {
+  backend "s3" {
+    bucket = "test123-example1-app1"
+    key = "terraform.tfstate"
+    dynamodb_table = "terraform_state"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+*/
+
+>>>>>>> Stashed changes
 resource "null_resource" "example" {}
